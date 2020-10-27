c8e4521c5df32b5766933b213e00ca9988c082e8
KMitzner9 <KMitzner9@gmail.com>
2020-10-27 13:25:12 -0700

    Initial commit

---
+++ If_Else.py
@@ -0,0 +1,17 @@
+
+
+num1 = 12
+key = False
+
+if num1 == 12:
+    if key:         #shorthand for if key == True, same as saying if True
+        print('Num1 is equal to 12 and they have the key')
+    else:
+        print('Num1 IS equal to 12 and they DO NOT have the key')
+elif num1 < 12:
+    print('Num1 is less than 12')
+else:
+    print('Num1 is not equal to 12')
+
+
+          
---
+++ printNum.py
@@ -0,0 +1,7 @@
+
+import ourModule #needs to be in same directory to work, otherwise need filepath
+
+if __name__ == "__main__":
+    results = ourModule.getNumbers(5, 9)
+    print(results)
+    
